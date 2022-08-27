c15edaf48d44fd046ac76932c32963ee7e67b1c4
Th2mas <tkhlebovitch@hotmail.com>
2022-08-27 23:06:38 +0200

    Extend the education entry with the thesis and optional exchange semesters

--- src/components/education-entry/EducationEntry.stories.tsx
+++ src/components/education-entry/EducationEntry.stories.tsx
@@ -11,8 +11,44 @@ export default {
 
 const Template: ComponentStory<typeof EducationEntry> = (args) => <EducationEntry {...args} />;
 
-export const Minf = Template.bind({});
-Minf.args = {
-  curriculum: "Media Informatics and Visual Computing",
-  university: "TU Vienna"
+export const StudyWithCompletion = Template.bind({});
+StudyWithCompletion.args = {
+  study: {
+    curriculum: "Media Informatics and Visual Computing",
+    university: "TU Vienna",
+    startDate: new Date(2014, 9)
+  },
+  title: "Bachelor of Science",
+  thesis: {
+    name: "An interactive dashboard approach for the visual analysis of rocket flight data",
+    link: "https://th2mas.github.io/pdf/FMS.pdf"
+  }
+};
+
+export const StudyOnGoing = Template.bind({});
+StudyOnGoing.args = {
+  study: {
+    curriculum: "Media Informatics and Visual Computing",
+    university: "TU Vienna",
+    startDate: new Date(2014, 9)
+  },
+  title: "Bachelor of Science"
+};
+
+export const StudyCompletedWithOneExchangeSemester = Template.bind({});
+StudyCompletedWithOneExchangeSemester.args = {
+  study: {
+    curriculum: "Electrical Engineering and Information Technology",
+    university: "TU Vienna",
+    startDate: new Date(2018, 9)
+  },
+  title: "Bachelor of Science",
+  exchangeSemesters: [
+    {
+      curriculum: "Industrial Engineering",
+      university: "Universitat Politècnica de València",
+      startDate: new Date(2020, 2),
+      endDate: new Date(2020, 6)
+    }
+  ]
 };
--- src/components/education-entry/EducationEntry.tsx
+++ src/components/education-entry/EducationEntry.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
-import './EducationEntry.scss';
+import './EducationEntry.css';
 
-interface EducationEntryProps {
+interface ThesisProps {
+  /**
+   * The name of the thesis written to complete the study
+   */
+  name: string;
+
+  /**
+   * The link to the thesis (should be publicly available)
+   */
+  link: string;
+}
+
+type Semester = 'Summer semester' | 'Winter semester';
+
+interface StudyProps {
+
   /**
    * The name of the curriculum
    */
@@ -11,13 +26,80 @@ interface EducationEntryProps {
    * The name of the university the curriculum is offered
    */
   university: string;
+
+  /**
+   * The date when the curriculum was started
+   * TODO: Can not be in the future!
+   */
+  startDate: Date;
+
+  /**
+   * The date when the curriculum was ended (not necessarily finished)
+   * TODO: Can not be in the future!
+   */
+  endDate?: Date;
 }
 
-export const EducationEntry = ({curriculum, university, ...props}: EducationEntryProps) => (
-  <div
-    {...props}
-  >
-    <h1>{curriculum}</h1>
-    <h2>{university}</h2>
-  </div>
-);
+interface EducationEntryProps {
+
+  /**
+   * The basic information of the curriculum
+   */
+  study: StudyProps;
+
+  /**
+   * Title that is received after successful completion
+   */
+  title: string;
+
+  /**
+   * The thesis written to complete the study
+   */
+  thesis?: ThesisProps;
+
+  /**
+   * All exchange semesters that were completed during the studies
+   */
+  exchangeSemesters?: Array<StudyProps>;
+}
+
+export const EducationEntry = ({study, title, thesis, exchangeSemesters}: EducationEntryProps) => {
+
+  const {curriculum, university, startDate, endDate} = study;
+
+  const start = startDate.getFullYear();
+  const end = endDate ? endDate.getFullYear() : 'Now';
+
+  const thesisComponent = thesis
+    ? <p>Thesis: <a href={thesis.link}>{thesis.name}</a></p>
+    : <></>;
+
+    // TODO: This should be its own component and should be properly tested
+  const exchangeSemesterComponent = exchangeSemesters
+    ? exchangeSemesters.map(study => {
+      const startYear = study.startDate.getFullYear();
+      const endYear = study.endDate?.getFullYear() || "Now";
+
+      const isSameYear: boolean = startYear === endYear;
+      const dateText = isSameYear ? startYear : `${startYear} - ${endYear}`;
+
+      return (
+        <div>
+          <h2>{study.university}</h2>
+          <h1>{study.curriculum}</h1>
+          <h3>{dateText}</h3>
+        </div>
+      )
+    }) : <></>;
+
+  return (
+    <div className="education-entry">
+      <h2>{university}</h2>
+      <h1>{curriculum}</h1>
+      <h3>{title}</h3>
+      <h3>{start} - {end}</h3>
+      {thesisComponent}
+      {exchangeSemesterComponent}
+    </div>
+  )
+};
